8bb4e82569e3e173a2055961d5f06678ffb00664
webcssphp <chengyongxiao123@126.com>
2016-09-07 22:46:54 +0800

    s

--- wxpay/index.php
+++ wxpay/index.php
@@ -1,36 +1,35 @@
 <?php
 header("Content-type: text/html; charset=utf-8");
+date_default_timezone_set('PRC');
+ini_set("display_errors", 0);
+error_reporting(E_ALL ^ E_NOTICE ^ E_STRICT);
+//error_reporting(E_ALL);
 
+//修正部份计算机 DOCUMENT_ROOT 无值的问题
+if(empty($_SERVER['DOCUMENT_ROOT']) && !empty($_SERVER['SCRIPT_FILENAME'])) {
+if(PATH_SEPARATOR==':') $_SERVER['DOCUMENT_ROOT'] = str_replace( '\\', '/', substr($_SERVER['SCRIPT_FILENAME'], 0, 0 - strlen($_SERVER['PHP_SELF'])));
+else $_SERVER['DOCUMENT_ROOT'] = substr($_SERVER['SCRIPT_FILENAME'], 0, 0 - strlen($_SERVER['PHP_SELF']));
+}else if(empty($_SERVER['DOCUMENT_ROOT']) && !empty($_SERVER['PATH_TRANSLATED'])) {
+    if(PATH_SEPARATOR==':') $_SERVER['DOCUMENT_ROOT'] = str_replace( '\\', '/', substr(str_replace('\\\\', '\\', $_SERVER['PATH_TRANSLATED']), 0, 0 - strlen($_SERVER['PHP_SELF'])));
+    else $_SERVER['DOCUMENT_ROOT'] = substr(str_replace('\\\\', '\\', $_SERVER['PATH_TRANSLATED']), 0, 0 - strlen($_SERVER['PHP_SELF']));
+}
+header("Content-type: text/html; charset=utf-8");
 if (get_magic_quotes_gpc()) {
- function stripslashes_deep($value){
-  $value = is_array($value) ?
-  array_map('stripslashes_deep', $value) :
-  stripslashes($value);
-  return $value;
- }
- $_POST = array_map('stripslashes_deep', $_POST);
- $_GET = array_map('stripslashes_deep', $_GET);
- $_COOKIE = array_map('stripslashes_deep', $_COOKIE);
+	function stripslashes_deep($value){
+		$value = is_array($value) ? array_map('stripslashes_deep', $value) : stripslashes($value); 
+		return $value; 
+	}
+	$_POST = array_map('stripslashes_deep', $_POST);
+	$_GET = array_map('stripslashes_deep', $_GET);
+	$_COOKIE = array_map('stripslashes_deep', $_COOKIE); 
 }
-define('APP_NAME', 'cms');
-define('CONF_PATH','./../Conf/');
-define('TMPL_PATH','./../tpl/');
-$GLOBALS['_beginTime'] = microtime(TRUE);
-define('MEMORY_LIMIT_ON',function_exists('memory_get_usage'));
-define('CORE','./../');
-if(MEMORY_LIMIT_ON) $GLOBALS['_startUseMems'] = memory_get_usage();
-define('APP_PATH','./../WQLCms/');
-defined('APP_PATH') 	or define('APP_PATH', dirname($_SERVER['SCRIPT_FILENAME']).'/');
-define('RUNTIME_PATH','./../Conf/logs/');
-defined('RUNTIME_PATH') or define('RUNTIME_PATH',APP_PATH.'Runtime/');
 define('APP_DEBUG',1);
-defined('APP_DEBUG') 	or define('APP_DEBUG',false);
-$runtime = defined('MODE_NAME')?'~'.strtolower(MODE_NAME).'_runtime.php':'~runtime.php';
-defined('RUNTIME_FILE') or define('RUNTIME_FILE',RUNTIME_PATH.$runtime);
-if(!APP_DEBUG && is_file(RUNTIME_FILE)) {
-    require RUNTIME_FILE;
-}else{
-    defined('THINK_PATH') or define('THINK_PATH', dirname(__FILE__).'/../');
-    require THINK_PATH.'Common/runtime.php';
-}
+define('APP_NAME', 'cms');
+define('CONF_PATH','../WQLData/Conf/');
+define('RUNTIME_PATH','../WQLData/logs/');
+define('TMPL_PATH','../tpl/');
+define('HTML_PATH','../WQLData/html/');
+define('APP_PATH','../WQLCms/');
+define('CORE','../WQLCms/_Core');
+require(CORE.'/WQLCms.php');
 ?>
